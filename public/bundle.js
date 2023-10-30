6d3c45ae6200ff8a57ee7677005a0e12cb20dfd5
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-10-30 13:49:15 +0200

    Update public

---
+++ public/bundle.js
@@ -0,0 +1,10 @@
+(() => {
+  var __webpack_modules__ = {
+    "./index.js": () => {
+      eval("\n\n//# sourceURL=webpack://portfolio/./index.js?");
+    },
+  };
+
+  var __webpack_exports__ = {};
+  __webpack_modules__["./index.js"]();
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./public/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "src"),
+      directory: path.join(__dirname, "public"),
     },
     port: 3000,
   },
